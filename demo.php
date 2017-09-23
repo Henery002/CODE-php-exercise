0085ff91296b915a56e4e83b48997852aba9bd05
Henery <Henery_002@163.com>
2017-09-23 12:49:59 +0800

    php-exercise

---
+++ demo.php
@@ -0,0 +1,23 @@
+<?php
+	$hd = '后盾网';
+	$url = 'http://ww.baidu.com';
+	$城市 = '南京';
+	if(empty($hd)){
+		echo "没有变量"."<br/>";
+	}else{
+		echo $hd."<br/>";
+	}
+
+	var_dump($url)."<br/>";
+	echo "$url"."<br/>";
+	echo $城市;
+
+	/**/
+	/*
+		$_GET[]
+		$_POST[]
+		$_REQUEST[]
+	*/
+
+
+?>
