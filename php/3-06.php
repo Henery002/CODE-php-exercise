3eb84539e56f7f90917e53291017a97f9a06bc0f
Henery <Henery_002@163.com>
2017-10-10 16:45:29 +0800

    3-07.php

--- php/3-06.php
+++ php/3-06.php
@@ -7,14 +7,14 @@
 * 使用反斜杠引用字符串
 */
 $str = "网易+云\课&*堂'视频教程'";
-echo addslashes($str);		//网易+云\\课&*堂\'视频教程\'
+echo addslashes($str);		//网易+云\\课&*堂\'视频教程\'   转义 '  "  \  等
 echo "<br/>";
 
 echo "<hr/>";
 
-phpinfo();					// 输出关于 PHP 配置的信息
-print_r(ini_get_all());
 echo "<pre>";
+print_r(ini_get_all());
+phpinfo();					// 输出关于 PHP 配置的信息
 
 
 ?>
---
+++ php/3-07.php
@@ -0,0 +1,22 @@
+<?php
+/*
+* 字符串处理函数六
+*/
+
+/*
+* get_magin_quotes_runtime();
+*/
+phpinfo();
+echo get_magic_quotes_runtime();
+
+
+
+
+
+
+
+
+
+
+
+?>
